a06427d78a39685dc53a0e687d80291158860954
nataki <nat.aki.nat.aki@gmail.com>
2016-02-29 00:28:50 +0200

    added formatting in different units, inherited and static methods

--- 02/HW/Shape.js
+++ 02/HW/Shape.js
@@ -1,25 +1,75 @@
 /**
  * Created by nataki on 2/20/16.
  */
-class Shape {
-    constructor(){
 
+/*
+
+ Calculator of perimeter and area of geometric shapes
+ Use ES6 classes to create a base class for geometric shapes with the possibility of calculating perimeter,
+ area and format the output of these values (in millimeters/centimeter/meters for perimeter and millimeters²/centimeters²/meters² for area).
+ After create classes for the Rectangle, Circle, Triangle and Trapezoid. They have to use the general functionality from the base class and have to include only the specific code for specific shape.
+
+ */
+'use strict';
+
+class Shape {
+
+    constructor(type){
+        this._type = type;
     }
     getPerimeter() {
-
+        return 0;
     }
 
     getArea() {
-
+        return 0;
     }
 
-    static format(value, metrics) {
+    printPerimeter(units) {
+        units = units || 'mm';
+        console.log(`${this._type} perimiter in ${units}: ${Shape.formatPerimeter(this.getPerimeter(), units)}`);
+    }
 
+    printArea(units) {
+        units = units || 'mm';
+        let area = Shape.formatArea(this.getArea(), units);
+        console.log(`${this._type} area in ${units}: ${area}`);
+    }
+
+    static formatPerimeter(value, units) {
+        let formattedValue = value;
+        switch (units) {
+            case 'm':
+                formattedValue = value / 1000;
+                break;
+            case 'cm':
+                formattedValue = value / 100;
+                break;
+            case 'mm':
+            default:
+        }
+        return formattedValue;
+    }
+
+    static formatArea(value, units) {
+        let formattedValue = value;
+        switch (units) {
+            case 'm':
+                formattedValue = value / (1000 * 1000);
+                break;
+            case 'cm':
+                formattedValue = value / (100 * 100);
+                break;
+            case 'mm':
+            default:
+        }
+        return formattedValue;
     }
 }
 
 class Rectangle extends Shape {
     constructor(a, b) {
+        super('Rectangle');
         this.width = a;
         this.height = b;
     }
@@ -29,11 +79,11 @@ class Rectangle extends Shape {
     getArea() {
         return this.width * this.height;
     }
-
 }
 
 class Circle extends Shape {
     constructor(r) {
+        super('Circle');
         this.r = r;
     }
     getPerimeter() {
@@ -45,6 +95,7 @@ class Circle extends Shape {
 }
 class Triangle extends Shape {
     constructor(a, b, c) {
+        super('Triangle');
         this.sides = [a, b, c];
     }
     getPerimeter() {
@@ -55,8 +106,10 @@ class Triangle extends Shape {
         return Math.sqrt(this.sides.reduce(function(v, item){ return (s-item)*v},s));
     }
 }
+
 class Trapezoid extends Shape {
     constructor(b1, b2, h) {
+        super('Trapezoid');
         this.b1 = b1;
         this.b2 = b2;
         this.h = h;
@@ -64,7 +117,16 @@ class Trapezoid extends Shape {
     getPerimeter() {
         return this.b1 + this.b2 + this.h;
     }
+
     getArea() {
-        return this.a*(this.b1 + this.b2)/2;
+        return this.h *(this.b1 + this.b2)/2;
     }
+}
+
+
+module.exports = {
+    Rectangle: Rectangle,
+    Trapezoid: Trapezoid,
+    Circle: Circle,
+    Triangle: Triangle
 }
---
+++ 02/HW/index.js
@@ -0,0 +1,42 @@
+/**
+ * Created by nataki on 2/28/16.
+ */
+var shapes = require('./Shape');
+
+console.log("--- RECTANGLE TEST ---");
+var rect = new shapes.Rectangle(100, 50);
+console.log(rect.getArea());
+console.log(rect.getPerimeter());
+
+rect.printArea('m');
+rect.printArea('cm');
+rect.printArea();
+
+rect.printPerimeter('m');
+rect.printPerimeter('cm');
+rect.printPerimeter();
+
+console.log("--- CIRCLE TEST ---");
+var circle = new shapes.Circle(15);
+
+circle.printArea('cm');
+circle.printArea();
+circle.printPerimeter('cm');
+circle.printPerimeter();
+
+console.log("--- trapezoid TEST ---");
+var triangle = new shapes.Triangle(10, 30,30);
+
+triangle.printArea('cm');
+triangle.printArea();
+triangle.printPerimeter('cm');
+triangle.printPerimeter();
+
+console.log("--- TRAPEZOID TEST ---");
+var trapezoid = new shapes.Trapezoid(10, 30,30);
+
+trapezoid.printArea('cm');
+trapezoid.printArea();
+trapezoid.printPerimeter('cm');
+trapezoid.printPerimeter();
+
